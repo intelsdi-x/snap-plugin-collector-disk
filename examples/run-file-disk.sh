651ddb8e37acadd3ceb72c201b594c4e36f4dbed
Marcin Krolik <marcin.krolik@intel.com>
2016-12-14 14:43:43 +0100

    Reorganized example and updated large tests

--- examples/run-file-disk.sh
+++ examples/run-file-disk.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 # get the directory the script exists in
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__proj_dir="$(cd $__dir && cd ../../ && pwd)"
+__proj_dir="$(cd $__dir && cd ../ && pwd)"
 __proj_name="$(basename $__proj_dir)"
 
 export PLUGIN_SRC="${__proj_dir}"
@@ -15,7 +15,7 @@ export PLUGIN_SRC="${__proj_dir}"
 . "${__proj_dir}/scripts/common.sh"
 
 # verifies dependencies
-. "${__proj_dir}/examples/tasks/.setup.sh"
+. "${__proj_dir}/examples/.setup.sh"
 
 # downloads plugins, starts snap, load plugins and start a task
-cd "${__proj_dir}/examples/tasks" && docker-compose exec main bash -c "PLUGIN_PATH=/etc/snap/plugins /${__proj_name}/examples/tasks/mock-disk.sh && printf \"\n\nhint: type 'snaptel task list'\ntype 'exit' when your done\n\n\" && bash"
+cd "${__proj_dir}/examples/tasks" && docker-compose exec main bash -c "PLUGIN_PATH=/etc/snap/plugins /${__proj_name}/examples/file-disk.sh && printf \"\n\nhint: type 'snaptel task list'\ntype 'exit' when your done\n\n\" && bash"
